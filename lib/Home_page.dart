e9d33d57150337585a8f4a308c8024664d9ea715
LINDEX171 <lindex171@gmail.com>
2024-11-13 16:56:38 +0000

    adding the container image with border radius

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,53 +11,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(244, 243, 243, 1),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          //brightness : Brightness.light,
-           leading: IconButton(
-             icon: Icon(Icons.menu,),
-             onPressed: (){},
-           ),
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        //brightness : Brightness.light,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+          ),
+          onPressed: () {},
         ),
+      ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(20))),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'find your',
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'inspiration',
+                  style: TextStyle(color: Colors.black, fontSize: 40),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  width: double.infinity,
+                  padding: EdgeInsets.all(5),
                   decoration: BoxDecoration(
-                    color: Colors.white
+                      color: Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: "search you're looking for",
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
                   ),
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "promo Today",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15,),
+                Container(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
                     children: [
-                      Text('find your', style: TextStyle(color: Colors.black,fontSize: 25),),
-                      SizedBox(height: 3,),
-                      Text('inspiration', style: TextStyle(color: Colors.black,fontSize: 40),),
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(244, 243, 243, 1),
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search,color: Colors.black87,),
-                            hintText: "search you're looking for"
-                          ),
-                        ),
-
-                      )
+                      promoCard('')
                     ],
                   ),
                 )
               ],
-        )
-         
+            ),
+          )
+        ],
+      )),
+    );
+  }
+
+  Widget promoCard(image){
+    return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(20)
+        ),
       ),
     );
   }
